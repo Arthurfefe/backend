f14a85bc9b1a2b14858abb2ea70617b19c03fae0
fefe <arthurfernandesdias2@gmail.com>
2025-09-30 16:28:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require("pg");
 const cors = require("cors");
-
+const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
@@ -15,6 +15,16 @@ const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir); // cria a pasta se não existir
 }
+
+// --- Configuração Multer ---
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, uploadDir),
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage });
 
 // --- Configuração do Postgres ---
 const pool = new Pool({
